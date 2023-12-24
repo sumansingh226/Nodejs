d8ac9359293a30ff5420346bd4e1bc606f5110a7
sumansingh226 <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-12-24 23:45:00 +0530

    send  html form in response

--- app.js
+++ app.js
@@ -2,13 +2,44 @@ const http = require("http");
 const { system } = require("nodemon/lib/config");
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers, req.url, req.method)
-    res.setHeader('Content-type', 'text/html')
-    res.write('<html');
-    res.write('<h1>Hello suman</h1')
-    res.write('</html');
+    const { url } = req;
+    if (url === "/") {
+        res.setHeader("Content-type", "text/html");
+        res.write("<html");
+        res.write("<h1>Home Page</h1");
+        res.write("</html");
+    } else {
+        res.setHeader("Content-type", "text/html");
+        res.write("<html");
+        res.write(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <title>Login Form</title>
+        </head>
+        <body>     
+        <h2>Login</h2>     
+        <form action="/submit" method="post">
+          <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+          </div>
+          <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+          </div>
+          <div>
+            <input type="submit" value="Submit">
+          </div>
+        </form>
+        
+        </body>
+        </html>
+        `);
+        res.write("</html");
+    }
+
     res.end();
-})
+});
 
-
-server.listen(4000)
+server.listen(4000);
