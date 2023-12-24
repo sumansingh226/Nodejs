1d6c359391f3507c38d110417596a589ff55500f
sumansingh226 <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-12-24 23:34:17 +0530

    send response to browser

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const http = require("http");
+const { system } = require("nodemon/lib/config");
 
 const server = http.createServer((req, res) => {
-
+    console.log(req.headers, req.url, req.method)
+    res.setHeader('Content-type', 'text/html')
+    res.write('<html');
+    res.write('<h1>Hello suman</h1')
+    res.write('</html');
+    res.end();
 })
+
+
+server.listen(4000)
