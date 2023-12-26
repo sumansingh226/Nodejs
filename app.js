d410ac18b2050f64a8a554367ff22a139fc185b3
sumansingh226 <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-12-27 02:22:49 +0530

    middlewahe

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("middlewhare");
+    next();
+})
+
 // Define a route
 app.get('/', (req, res) => {
     res.send('Hello, Express!');
