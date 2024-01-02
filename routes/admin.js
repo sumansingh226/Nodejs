314bb76aa62b3dde0ec7fff3f1f11ec5289b7aec
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-02 23:39:41 +0530

    chnage req method

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,11 @@ const path = require("path");
 
 const Router = express.Router();
 // Define a route
-Router.get("/add-product", (req, res, next) => {
+Router.post("/add-product", (req, res, next) => {
     res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 Router.get("/product", (req, res, next) => {
-    console.log(req.body);
     res.redirect("/");
 });
 module.exports = Router;
