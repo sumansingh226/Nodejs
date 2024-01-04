0cf97dea6d2995a7d77e0474bd40c20373eddd72
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-05 01:08:51 +0530

    code format

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
 
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
