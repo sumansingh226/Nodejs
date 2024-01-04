ea580e9dd3ecc3729f54bf550703168d1b2bf7b6
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-05 00:33:59 +0530

    define seprate controllerfor logic

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,15 @@
 const path = require('path');
 
 const express = require('express');
-
-const rootDir = require('../util/path');
+const { addProducts, getAddProducts, postAddProducts } = require('../controller/products');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', getAddProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', postAddProducts);
 
 exports.routes = router;
 exports.products = products;
