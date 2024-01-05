a4aa4fcde94ade2cec5d830373803fc3282cffe4
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-06 02:09:38 +0530

    refactore  folder structure and added more files to make e-shop

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -11,15 +11,16 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const payload = ({ title, description, price, quantity } = req.body);
+  const productID = new Date().getTime();
+  const payload = ({ title, description, price, quantity, } = req.body);
 
-  const product = new Product({ ...payload });
+  const product = new Product({ ...payload, productID });
   product.save();
   res.redirect("/");
 };
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
