e57847f141d3cb3faae00511ee99beea322b3cc7
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-07 23:46:16 +0530

    fix total prise and qty bug

--- models/cart.js
+++ models/cart.js
@@ -1,33 +1,36 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(path.dirname(process.mainModule.filename), "data", "cart.json");
+const filePath = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "cart.json"
+);
+
 module.exports = class Cart {
-    static addToCart(id, productPrize) {
+    static addToCart(id, productPrice) {
         fs.readFile(filePath, (err, file) => {
-            let cart = { products: [], totalPrize: 0 };
+            let cart = { products: [], totalPrice: 0 };
             if (!err) {
-                cart = JSON.parse(file) ?? [];
+                cart = JSON.parse(file) || cart;
             }
+
             const existingProductIndex = cart.products.findIndex(
                 (prod) => prod.productID == id
             );
 
-            const existingProduct = cart.products.find[existingProductIndex];
-
-            let updatedProduct;
-            if (existingProduct) {
-                updatedProduct = { ...existingProduct };
-                updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products = [...cart.products];
-                cart.products[existingProductIndex] = updatedProduct;
+            if (existingProductIndex !== -1) {
+                cart.products[existingProductIndex].qty += 1;
             } else {
-                updatedProduct = { productID: id, qty: 1 };
-                cart.products = [...cart.products, updatedProduct];
+                cart.products.push({ productID: id, qty: 1 });
             }
-            cart.totalPrize = cart.totalPrize + productPrize;
-            fs.writeFile(filePath, JSON.stringify(cart.products), (err) => {
-                console.log(err);
+
+            cart.totalPrice += +productPrice; // Adding product price to total price
+
+            fs.writeFile(filePath, JSON.stringify(cart), (err) => {
+                if (err) {
+                    console.log(err);
+                }
             });
         });
     }
