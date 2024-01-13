4e96e291911bb9902eee1800e69990e59a8862fa
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-14 01:40:10 +0530

    connection to db

---
+++ db/mySqlDbConntection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+// Replace these with your MySQL connection details
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Chauhan@#$1234',
+    database: 'nodejsmysql',
+});
+
+module.exports = connection.promise()
