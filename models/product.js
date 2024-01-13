bcd7b76b8d870f625aabcb040d39133d8e5dabb9
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-14 02:51:44 +0530

    saving data into tnaleinsert query

--- models/product.js
+++ models/product.js
@@ -1,17 +1,28 @@
-const db = require("../db/mySqlDbConntection")
+const db = require("../db/mySqlDbConntection");
 
 module.exports = class Product {
     constructor(productsInformation) {
         this.productsInformation = productsInformation;
     }
 
-    save() { }
+    save() {
+        return db.execute(
+            "INSERT INTO products (title, image, price, quantity, description) VALUES (?, ?, ?, ?, ?)",
+            [
+                this.productsInformation.title,
+                this.productsInformation.image,
+                this.productsInformation.price,
+                this.productsInformation.quantity,
+                this.productsInformation.description,
+            ]
+        );
+    }
 
     Edit(id) { }
     static deleteProduct(id) { }
 
     static fetchAll() {
-        return db.execute('SELECT * FROM products')
+        return db.execute("SELECT * FROM products");
     }
 
     static fetchById(id) { }
