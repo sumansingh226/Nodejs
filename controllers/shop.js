5c305bbf2abf8b82c2f3878fa0528acf47f3a96e
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-27 00:08:52 +0530

    define order schema

--- controllers/shop.js
+++ controllers/shop.js
@@ -131,7 +131,7 @@ exports.postCheckout = (req, res, next) => {
   await User.findById(req.user._id)
     .populate("cart.items.productID")
     .exec().then((user) => {
-
+      const products = user.cart.items;
       const order = new Order({
         user: {
           name: req.user.name,
