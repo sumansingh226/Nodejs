7cb2b0f99f1a5e488ebb9ef10b485a6148eb8fde
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-27 23:19:42 +0530

    adding login page

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,6 @@
 const express = require("express");
 
-
 const router = express.Router();
 
-router.get("/login", (req, res, next) => {
-
-})
+router.get("/login", (req, res, next) => { });
 module.exports = router;
