166ab33f61bdfc28e8d2f2873cc99e9f25ac1d28
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-30 00:28:17 +0530

    logut controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,23 +4,21 @@ exports.getLogin = (req, res, next) => {
     res.render("auth/login", {
         path: "/login",
         pageTitle: "Login",
-        isAuthenticated: req.IsLoggedIn
+        isAuthenticated: req.IsLoggedIn,
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10; HttpOnly')
-    monggoseUserModel.findById('65b2d761f9c61f421b37f9de')
-        .then(user => {
-            console.log(user);
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            res.redirect('/');
-        })
+    res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10; HttpOnly");
+    monggoseUserModel.findById("65b2d761f9c61f421b37f9de").then((user) => {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        res.redirect("/");
+    });
 };
 
 exports.postLogOut = (req, res, next) => {
-    req.session.destroy(err => {
+    req.session.destroy((err) => {
         if (err) {
             console.error("Error destroying session:", err);
             return next(err); // Forward the error to the error-handling middleware
