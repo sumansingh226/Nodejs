49a0c33af1fde6ca7ca1c731bc16dd142402cd5a
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-29 22:57:38 +0530

    initilization of session

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const User = require("./models/monggoseUserModel");
 const port = process.env.PORT || 3000; // Use the provided PORT or default to 3000
 const mongoose = require("mongoose");
 require("dotenv").config();
+const session = require("express-session")
 
 app.set("view engine", "ejs");
 app.set("views", "views");
