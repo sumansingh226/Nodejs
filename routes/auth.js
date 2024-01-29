8744dbc1e6eec0b21cd9e31df92c5b166b1b9b4a
sumansingh226 <sumansingh13062000@gmail.com>
2024-01-29 23:46:33 +0530

    initilization of session-mongodb

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
-
 router.post("/logout", authController.postLogOut);
 
 module.exports = router;
