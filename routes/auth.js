51b04c58efd0179404cee6abca5030ef2e30ccb4
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-05 00:08:59 +0530

    update password

--- routes/auth.js
+++ routes/auth.js
@@ -10,5 +10,6 @@ router.post("/logout", authController.postLogOut);
 router.get("/reset-password", authController.getResetPassword)
 router.post("/reset-password", authController.postResetPassword)
 router.get("/update-password/?:token", authController.getUpdatePassword)
+router.post("/update-password/?:token", authController.postUpdatePassword)
 
 module.exports = router;
