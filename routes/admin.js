681df4cc7a0c6bced49c6f301e4c1fc64f9343c4
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-07 00:21:20 +0530

    refactor the code

--- routes/admin.js
+++ routes/admin.js
@@ -16,6 +16,6 @@ router.get("/edit-product/:productID", isAuth, adminController.getEditProduct);
 
 router.post("/edit-product", isAuth, adminController.postEditProduct);
 
-router.post("/delete-product", adminController.postDeleteProduct)
+router.post("/delete-product", isAuth, adminController.postDeleteProduct)
 
 module.exports = router;
