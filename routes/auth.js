8086119a8e50b3757e626a35993b8c5ad29724d2
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-12 23:49:29 +0530

    express validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/auth");
 const router = express.Router();
+const validator = require("express-validator");
 
 // Signup Routes
 router.get('/signup', authController.getSignUp);
