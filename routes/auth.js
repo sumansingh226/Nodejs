0283b78207de9254e7c5baf8a65c0dd22864e94e
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-13 00:13:47 +0530

    send a custom message

--- routes/auth.js
+++ routes/auth.js
@@ -4,22 +4,26 @@ const router = express.Router();
 const { check } = require("express-validator");
 
 // Signup Routes
-router.get('/signup', authController.getSignUp);
-router.post('/signup', check('email').isEmail(), authController.postSignUp);
+router.get("/signup", authController.getSignUp);
+router.post(
+    "/signup",
+    check("email").isEmail().withMessage("Please Entera A Valid Email Address"),
+    authController.postSignUp
+);
 
 // Login Routes
-router.get('/login', authController.getLogin);
-router.post('/login', authController.postLogin);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
 
 // Logout Route
-router.post('/logout', authController.postLogOut);
+router.post("/logout", authController.postLogOut);
 
 // Reset Password Routes
-router.get('/reset-password', authController.getResetPassword);
-router.post('/reset-password', authController.postResetPassword);
+router.get("/reset-password", authController.getResetPassword);
+router.post("/reset-password", authController.postResetPassword);
 
 // Update Password Routes
-router.get('/update-password', authController.getUpdatePassword);
-router.post('/update-password', authController.postUpdatePassword);
+router.get("/update-password", authController.getUpdatePassword);
+router.post("/update-password", authController.postUpdatePassword);
 
 module.exports = router;
