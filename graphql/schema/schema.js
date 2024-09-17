263adbfe233a1e8269ae7394e31145c90d1fceb1
sumansingh226 <sumansingh13062000@gmail.com>
2024-09-17 15:11:59 +0530

    node-graphql  mutation

--- graphql/schema/schema.js
+++ graphql/schema/schema.js
@@ -2,41 +2,40 @@ const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
     type TestData {
-        text: String!,
+        text: String!
         views: Int!
     }
     type RootQuery {
         hello: TestData
     }
     type Post {
-        _id : ID!
-        title : String!
-        content : String!
-        imageUrl : String!
-        creator : User 
-        createdAt : String!
-        updatedAt : String!
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User
+        createdAt: String!
+        updatedAt: String!
     }
     type User {
-        _id : ID!
-        name : String!
-        email : String!
-        password : String!
-        status : String!
-        posts : [Post!]!
+        _id: ID!
+        name: String!
+        email: String!
+        password: String!
+        status: String
+        posts: [Post!]!
     }
-    input userInputData {
-            email : String!
-            name : String!
-            password : String!
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
     }
-
     type RootMutation {
-        addUser( userInput : userInputData) : User!
+        addUser(userInput: UserInputData): User!
     }
     schema {
         query: RootQuery
-        mutation : RootMutation
+        mutation: RootMutation
     }
 `);
 
